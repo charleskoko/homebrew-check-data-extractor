c1f76c29afd790f0376f5977d1040cc03efa4f12
Charles Koko <55494612+charleskoko@users.noreply.github.com>
2024-10-07 14:05:42 +0200

    Update check-data-extractor.rb

--- check-data-extractor.rb
+++ check-data-extractor.rb
@@ -2,7 +2,7 @@ class CheckDataExtractor < Formula
   desc "Allows retrieving transaction information from the database."
   homepage "https://github.com/charleskoko/check-data-extractor"
   url "https://github.com/charleskoko/check-data-extractor/archive/refs/tags/v1.2.3.tar.gz"
-  sha256 "le_checksum_du_tar_gz"
+  sha256 "080f077a4a1237e438301059d9f2e70a8d22092fd6cd8d70fa9de7abfd1feba7"
   version "1.2.0"
 
   depends_on "php"
